8cbc171f9a904b042f841f8ded4701e069e45a9c
Dan Lister <d.lister@agebase.co.uk>
2014-02-25 22:58:19 +0000

    Added build and package msbuild script contents to create package zip file.

--- build/Release.bat
+++ build/Release.bat
@@ -1,1 +1,33 @@
-{\rtf1}
+@echo off
+
+IF .%1 == . (
+	@echo off
+	set /p VERSION="Enter version number (eg. 3.2.1): " %=%
+	@echo on
+)
+IF NOT .%1 == . (
+	set VERSION=%1
+)
+
+msbuild Build.msbuild
+set BUILD_STATUS=%ERRORLEVEL% 
+
+if %BUILD_STATUS%==0 goto continuebuild 
+if not %BUILD_STATUS%==0 goto failbuild 
+ 
+:failbuild
+exit /b 1 
+
+:continuebuild
+
+msbuild Package.msbuild
+set BUILD_STATUS=%ERRORLEVEL% 
+
+if %BUILD_STATUS%==0 goto end 
+if not %BUILD_STATUS%==0 goto failpackage 
+ 
+:failpackage
+exit /b 1 
+
+:end
+exit /b 0 
